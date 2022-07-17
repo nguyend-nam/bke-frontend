7656529e81b31e365b70e64778c8156704577fe9
nguyend-nam <nguyennamnade22@gmail.com>
2022-07-17 14:39:41 -0700

    chore: update gitignore

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfigVal = {
+  API_KEY: "AIzaSyDyLomb5hohlGICWfJBZxY_pijNgqWSVvo",
+  AUTH_DOMAIN: "bkenglish-auth.firebaseapp.com",
+  PROJECT_ID: "bkenglish-auth",
+  STORAGE_BUCKET: "bkenglish-auth.appspot.com",
+  MESSAGING_SENDER_ID: "1091985353466",
+  APP_ID: "1:1091985353466:web:fa5856b509c5ac514af743",
+};
